6015502c196076c274ddecacd041a2510390f54c
dixeris <hs031545@gmail.com>
2023-10-15 03:33:53 +0000

    add multithreading

--- chat/chat_server.c
+++ chat/chat_server.c
@@ -3,16 +3,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #include "socketheader.h"
 
 #define MAXDATASIZE 100
 
-void sigchld_handler(int s) {
-	int saved_errno = errno;
-	while(waitpid(-1,NULL,WNOHANG) >0);
-	errno = saved_errno;
-}
 
 void *get_in_addr(struct sockaddr *sa) {
 	if(sa->sa_family == AF_INET) {
@@ -20,20 +16,51 @@ void *get_in_addr(struct sockaddr *sa) {
 	}
 }
 
+void *clientHandler(void* void_sockfd) {
+	int new_fd;
+	struct sockaddr_storage theiraddr;
+	char s[INET6_ADDRSTRLEN];
+	socklen_t sin_size;
+	int sockfd  = *(int*)void_sockfd;
+	char buf[MAXDATASIZE];
+	int strlen;
+
+	while (1) {
+	
+		sin_size = sizeof(theiraddr);
+		new_fd = accept(sockfd, (struct sockaddr*)&theiraddr, &sin_size);
+		if(new_fd == -1) {
+			perror("accept");					
+			continue;
+			} 
+		
+		inet_ntop(theiraddr.ss_family, get_in_addr((struct sockaddr*)&theiraddr),s, sizeof(s));
+		printf("server connections from: %s\n", s);
+		while((strlen=read(new_fd,buf,MAXDATASIZE)) > 0) {
+			printf("received: %s\n", buf); 
+			if((strncmp(buf,"quit",4) == 0 ) || (strncmp(buf,"exit",4 == 0)))  
+				{ close(new_fd); pthread_exit(NULL); break; }
+			 write(new_fd,buf,MAXDATASIZE); 	
+			if(strlen<0) { close(new_fd); printf("read error"); }
+		}		
+		close(new_fd);
+	}
+}
+
+
+	
+
+
 
 int main(void) {
-	int sockfd, new_fd; //listen on sockfd, new connection on new_fd
+	int i;
+	int sockfd; //listen on sockfd, new connection on new_fd
 	struct addrinfo hints, *servinfo;
-	struct sockaddr_storage theiraddr;
 	int rv;
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags	 = AI_PASSIVE;
-	char s[INET6_ADDRSTRLEN];
-	struct sigaction sa;
 	int yes = 1;
-	socklen_t sin_size;
-	char buf[MAXDATASIZE];
 
 	if((rv = getaddrinfo(NULL,"3396",&hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
@@ -56,29 +83,17 @@ perror("server: socket");
 	if(listen(sockfd, 10) == -1) {
 		perror("listen");
 	}
-	while(1) {
-	sin_size = sizeof(theiraddr);
-	new_fd=accept(sockfd, (struct sockaddr *)&theiraddr, &sin_size);
-	if(new_fd==-1) {
-		perror("accept");
-		continue;
+	pthread_t thread[10];
+	for(i = 0; i<10; i++) {
+		pthread_create(&thread[i], NULL, clientHandler, (void*)&sockfd);
 		}
 	
-	
-	sa.sa_handler = sigchld_handler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if(sigaction(SIGCHLD, &sa, NULL) == -1) {
-		perror("sigaction");
-		exit(1);
-	}
-	inet_ntop(theiraddr.ss_family, get_in_addr((struct sockaddr*)&theiraddr),s, sizeof(s));
-	printf("server connections from: %s\n", s);
-	int strlen;
-	while((strlen=read(new_fd,buf,MAXDATASIZE)) > 0)  write(new_fd,buf,MAXDATASIZE); 
-	if(strlen<0) { close(new_fd); printf("read error"); }
-       	
 
+	for(i = 0; i<10; i++) {
+		pthread_join(thread[i],NULL);
 		}
-		}
+	return 0;
+
+}
+
 
